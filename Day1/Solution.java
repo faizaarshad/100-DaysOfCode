8f7381601a30716bdba0afd6304f989271eb3b8d
Faiza Arshad <159601350+faizaarshad@users.noreply.github.com>
2025-03-17 12:58:30 +0530

    Update Solution.java

--- Day1/Solution.java
+++ Day1/Solution.java
@@ -1,1 +1,22 @@
-"// Solution for Day 1" 
+// 🚀 Day 1 - Divide Array Into Equal Pairs Solution  
+import java.util.HashMap;
+
+class Solution {
+    public boolean divideArray(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count occurrences of each number
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Check if all numbers have even occurrences
+        for (int count : map.values()) {
+            if (count % 2 != 0) { // If any number appears an odd number of times, pairing is impossible
+                return false;
+            }
+        }
+
+        return true; // If all numbers appear an even number of times, they can be paired
+    }
+}
